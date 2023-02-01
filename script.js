6277891ba9dd4a6c97799b85ef37c332db2020af
Mythili1242 <mythili.galidevara@motivitylabs.com>
2023-02-01 21:19:23 +0530

    mockup created for homepage

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log(categories);
+const data=categories.categories;
+console.log(data);
+
+for(let i=0;i<data.length;i++){
+    if(i>5){
+        break;
+    }
+    console.log(data[i].strCategory);
+}
+
+
+// data.forEach(function(category){
+    // console.log(category.strCategory)
+// })
+
+// for(let i=0;i<data.length;i++)
+// {
+    // console.log(data[i].strCategory)
+// }
+
+// for(i=1;i<=100;i++){
+    // console.log(i);
+// }
+
+
+
+
